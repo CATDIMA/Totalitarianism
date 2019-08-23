d9d180b1275386821c3d5c3df1f1ef0cd456e8a7
CATDIMA <48386638+CATDIMA@users.noreply.github.com>
2019-08-23 22:14:23 +0300

    Приделал библиотеки
    Пиздец, что с SFML сделали, приходится .dll к коду закидывать. Это очень давит на психику, ладно хоть гаражный панк-рок пока спасает.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
+#include <string>
+#include "Box2D/Box2D.h"
+#include "SFML/Graphics.hpp"
 
 using namespace std;
+using namespace sf;
+
+const float SCALE = 30.0f;
+const float DEG = 57.29577f;
+int ResolutionX = 640;
+int ResolutionY = 480;
+bool IsRunning = true;
+bool Vsync = true;
+
+b2Vec2 NoGravity(0.0f, 0.0f);
+b2World World(NoGravity);
 
 int main()
 {
-	cout<< "I am a void file!" <<endl;
-	system("pause");
+	RenderWindow window(VideoMode(ResolutionX, ResolutionY), "Totalitarianism", Style::Close);
+
+	while (IsRunning)
+	{
+		//ьн?
+	}
+
 	return 0;
 }
